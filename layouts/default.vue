6813f65e7c1272e8e59bc53ef52b905598bcb0f0
Vino Samari <iamvinomusic@gmail.com>
2024-02-03 07:47:20 +0000

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-app class="Poppins">
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="$vuetify.breakpoint.mobile ? false : miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+      color="rgba(6,0,104,0.85)"
+      class="white--text"
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          class="white--text"
+        >
+          <v-list-item-action>
+            <v-icon color="purple">{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="font-weight-medium">{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      color="#a30367"
+      :clipped-left="clipped"
+      fixed
+      app
+      class="white--text"
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="white--text"/>
+      <v-btn
+        v-if="!$vuetify.breakpoint.mobile"
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-spacer/>
+
+      <v-toolbar-title>Happy Home Care</v-toolbar-title>
+      <v-spacer/>
+
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <footer-component></footer-component>
+  </v-app>
+</template>
+
+<script>
+import FooterComponent from "../components/FooterComponent.vue";
+
+export default {
+  name: 'DefaultLayout',
+  components: {FooterComponent},
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Home',
+          to: '/'
+        },
+        {
+          icon: 'mdi-head-snowflake',
+          title: 'Mental Health',
+          to: '/mental-health'
+        },
+        {
+          icon: 'mdi-weight-lifter',
+          title: 'Health and Wellness',
+          to: '/wellness'
+        },
+        {
+          icon: 'mdi-human-male-female-child',
+          title: 'Relationships',
+          to: '/relationships'
+        },
+        {
+          icon: 'mdi-gift',
+          title: 'Donate',
+          to: '/donate'
+        },
+        {
+          icon: 'mdi-account-multiple',
+          title: 'Team',
+          to: '/team'
+        },
+        {
+          icon: 'mdi-face-agent',
+          title: 'Contact us',
+          to: '/contact'
+        },
+
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Dokdo&family=Nanum+Gothic+Coding&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+
+.ubuntu-mono {
+  font-family: 'Ubuntu Mono', monospace;
+}
+
+.Natum-Gothic-Coding {
+  font-family: 'Nanum Gothic Coding', monospace;
+}
+
+.Poppins {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <img src="../assets/img/logo.png" alt="logo" width="100%" class="mx-auto" height="320"/>
+      <v-carousel :show-arrows="false" cycle height="70vh" hide-delimiter-background>
+        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+          <v-container fill-height fluid>
+            <v-row align="center" justify="center" class="fill-height">
+              <v-col>
+                <v-row justify="center" align="center" class="fill-height">
+                  <v-col>
+                    <v-card class="text-center mx-auto" color="rgba(255,255,255,0.5)" elevation="3" :width="$vuetify.breakpoint.mobile ? '70%' : '40%'" >
+                      <v-card-text class="black--text font-weight-bold elevation-2 px-5" style="font-size: 1.3rem">{{item.text}}</v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-carousel-item>
+      </v-carousel>
+      <v-card rounded elevation="3">
+        <v-card-title id="#services" class="font-weight-bold text-uppercase">Our services</v-card-title>
+        <features-card v-for="(feature, index) in services" :key="index" :title="feature.title" :description="feature.text"></features-card>
+      </v-card>
+    </v-col>
+  </v-row>
+
+</template>
+
+<script>
+import FeaturesCard from "../components/FeaturesCard.vue";
+
+export default {
+  name: 'IndexPage',
+  components: {FeaturesCard},
+  head:{
+    title: 'Home'
+  },
+  data() {
+    return {
+      items: [
+        {
+          src: require('../assets/img/IMG1.jpg'),
+          text: "Caring Hearts, Compassionate Care: Where Elders Become Family."
+        },
+        {
+          src: require('../assets/img/IMG2.jpg'),
+          text: "Nurturing Smiles, Creating Bonds: Your Trusted Elderly Care Partner."
+        },
+        {
+          src: require('../assets/img/IMG3.jpg'),
+          text: "Dignity in Every Detail: Providing Exceptional Care for Seniors.\""
+        },
+        {
+          src: require('../assets/img/IMG4.webp'),
+          text: "Cherishing Moments, Embracing Lives: A Home for Elderly Happiness."
+        },
+        {
+          src: require('../assets/img/IMG5.jpeg'),
+          text: "Warmth Beyond Words: Crafting a Legacy of Love for Our Elders."
+        },
+
+
+
+      ],
+      services:[
+        {
+          'title': 'Personal Care Assistance:',
+          'text': 'Providing support with activities of daily living, such as bathing, grooming, dressing, and assistance with mobility to ensure clients\' comfort and well-being.'
+        },
+        {
+          'title': 'Companion Care Services:',
+          'text': 'Offering companionship, social interaction, and emotional support through engaging activities, conversation, and accompaniment to appointments or outings.'
+        },
+        {
+          'title': 'Medication Management:',
+          'text': 'Ensuring proper administration of medications, adherence to prescribed schedules, and monitoring for any potential side effects or complications, with the aim of promoting health and safety for the clients.'
+        },
+
+      ]
+    }
+  }
+}
+</script>
